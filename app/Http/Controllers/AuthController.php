c5471e98a175457aa30d2f03c7c2ce4853f98c8f
Matheus <matheusmartinscia@gmail.com>
2025-05-28 21:51:21 -0300

    Feat: Adiciona lógica autenticação

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class AuthController extends Controller
@@ -14,6 +17,51 @@ class AuthController extends Controller
 
     public function authenticate (Request $request)
     {
-        echo "Authenticating...";
+        $credentials = $request->validate(
+            [
+                'username' => ['required', 'min:3', 'max:30'],
+                'password' => ['required', 'min:8', 'max:32', 'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/']
+            ],
+            [
+                'username.required' => "O username é obrigatório",
+                'username.min' => "O username deve ter no mínimo :min caracteres",
+                'username.max' => "O username deve ter no máximo :max caracteres",
+                'password.required' => "A senha é obrigatória",
+                'password.min' => "A senha deve ter no mínimo :min caracteres",
+                'password.max' => "A senha deve ter no máximo :max caracteres",
+                'password.regex' => "A senha deve ter uma letra maiúscula, uma minúscula e um dígito.",
+            ]
+        );
+
+        $user = User::where('username', $credentials['username'])
+            ->where('active', true)
+            ->where(function($query) {
+                $query->whereNull('blocked_until')
+                    ->orWhere('blocked_until', '<=', now());
+            })
+            ->whereNotNull('email_verified_at')
+            ->whereNull('deleted_at')
+            ->first();
+
+        if(!$user) {
+            return back()
+                ->withInput()
+                ->with("invalid_login", "Login inválido.");
+        }
+
+        if(!password_verify($credentials['password'], $user->password)) {
+            return back()
+                ->withInput()
+                ->with("invalid_login", "Login inválido.");
+        }
+        
+        $user->last_login = Carbon::now();
+        $user->blocked_until = null;
+        $user->save();
+        
+        $request->session()->regenerate();
+        Auth::login($user);
+
+        return redirect()->intended(route('home'));
     }
 }
