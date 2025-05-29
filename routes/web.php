be1e365cc793043d1ddad68866c37e243a3374a8
Matheus <matheusmartinscia@gmail.com>
2025-05-28 21:51:37 -0300

    Feat: Adiciona o name 'home' a rota '/'

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@ Route::middleware('guest')->group(function() {
 Route::middleware('auth')->group(function() {
     Route::get('/', function() {
         echo "bão";
-    });
+    })->name('home');
 });
