e398e6fe17f34a6ae0aa12e506301fc0ed303eca
Matheus <matheusmartinscia@gmail.com>
2025-06-01 20:42:27 -0300

    Feat: Cria rotas de registro e confirmação do email

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function() {
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/login', [AuthController::class, 'authenticate'])->name('auth');
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'storeUser'])->name('register.store');
+
+    Route::get('/register_confimartion/{token}', [AuthController::class, 'registerConfirmation'])->name('register.confirmation');
 });
 
 Route::middleware('auth')->group(function() {
