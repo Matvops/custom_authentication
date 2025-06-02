81245b4f3fe91fd336dbfb6b973b92155a81a7d2
Matheus <matheusmartinscia@gmail.com>
2025-06-01 21:01:11 -0300

    Feat: Cria view home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<x-layouts.main_layout title="Home">
+    <x-slot:content>
+        <div class="container-fluid mt-5">
+            <div class="row">
+                <div class="col p-3">
+                    <h1 class="text-center display-6">
+                        Welcome to the home page
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </x-slot:content>
+</x-layouts.main_layout>
