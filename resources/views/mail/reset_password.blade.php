a4a07258fd7bc3e6902deea5d72a646185aa66fb
Matheus <matheusmartinscia@gmail.com>
2025-06-02 21:30:42 -0300

    Feat: Cria conteúdo do email

---
+++ resources/views/mail/reset_password.blade.php
@@ -0,0 +1,4 @@
+<h3>Matheuzinho - Recuperação de senha</h3>
+<p>Caro <strong>{{ $username }}</strong>, para recuperar a senha de usuário, clique no link abaixo</p>
+<p><a href="{{ $link }}">Recuperar a senha</a></p>
+<p>Atenciosamente, <br>Equipe de suporte</p>
