d9de65e81c1d197b4b90a95fcbce2a53af3e5bbc
Matheus <matheusmartinscia@gmail.com>
2025-06-03 19:09:43 -0300

    Feat: Adiciona Trait SoftDelete

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as AuthUser;
 
 class User extends AuthUser
 {
+    use SoftDeletes;
 
     protected $hidden = [
         'password',
